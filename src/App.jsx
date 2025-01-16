2d06762cb790f4b3ce39db21f384f3ee30363a94
sumit1642 <sumeetyadav1642@gmail.com>
2025-01-17 00:04:30 +0530

    Added SpeedInsights for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // src/App.jsx
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import { Paste } from "./components/Paste";
@@ -39,6 +40,7 @@ const router = createBrowserRouter([
 function App() {
 	return (
 		<>
+			<SpeedInsights />
 			<Analytics />
 			<div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
 				<RouterProvider router={router} />
